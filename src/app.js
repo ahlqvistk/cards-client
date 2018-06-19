9cb53f1891cebcf3aec619264a9d97580e86838b
ahlqvistk <kristoffer.ahlqvist@gmail.com>
2018-06-19 08:56:02 +0200

    Add most

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 /* eslint no-console: 0 */
 /* global console, document */
+import {fromEvent} from 'most';
 import io from 'socket.io-client';
 
 const socket = io();
-socket.on('state', (data) => {
-    console.log(data);
-});
+const state$ = fromEvent('state', socket);
+state$.observe((state) => console.log(state));
 
 const el = document.getElementById('app');
 el.innerHTML = '<h1>Cards Client</h1>';
