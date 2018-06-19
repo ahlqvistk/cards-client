c19d6e4b299b55d4e093472fb217b08a36a3a99a
ahlqvistk <kristoffer.ahlqvist@gmail.com>
2018-06-19 09:42:04 +0200

    Add hyperapp

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 /* eslint no-console: 0 */
-/* global console, document */
+/* global document */
+import {h, app} from 'hyperapp';
 import {fromEvent} from 'most';
 import io from 'socket.io-client';
 
 const socket = io();
 const state$ = fromEvent('state', socket);
-state$.observe((state) => console.log(state));
 
-const el = document.getElementById('app');
-el.innerHTML = '<h1>Cards Client</h1>';
+const actions = () => {};
+
+const view = (state) => (
+  <h1>Cards Client</h1>
+);
+
+state$.observe((state) => {
+  app(state, actions, view, document.body);
+});
