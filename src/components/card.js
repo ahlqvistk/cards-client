32293063843efb7379533e5b2033d0814e5374dd
ahlqvistk <kristoffer.ahlqvist@gmail.com>
2018-07-11 18:18:24 +0200

    Allow players to play cards

--- src/components/card.js
+++ src/components/card.js
@@ -1,9 +1,18 @@
 import {h} from 'hyperapp';
+import {socket} from '../app';
 import {
   getRank,
   getSuit,
   getUnicodeSuit,
 } from '../../../cards-common/src/get-rank-suit';
+import validPlay from '../../../cards-common/src/valid-play';
+
+function playCard(card) {
+  return socket.emit('action', {
+    type: 'play card',
+    payload: {playedCard: card},
+  });
+}
 
 export const Card = ({card}) => (state) => {
   if (card === 'back') {
@@ -17,13 +26,23 @@ export const Card = ({card}) => (state) => {
   const rank = getRank(card);
   const suit = getSuit(card);
   const unicodeSuit = getUnicodeSuit(card);
+  const valid = validPlay(card, state);
 
   return (
     <li>
-      <a class={`card rank-${rank} ${suit}`}>
-        <span class='rank'>{rank}</span>
-        <span class='suit'>{unicodeSuit}</span>
-      </a>
+      {valid ?
+        <a
+          class={`card rank-${rank} ${suit}`}
+          onclick={() => playCard(card)}
+        >
+          <span class='rank'>{rank}</span>
+          <span class='suit'>{unicodeSuit}</span>
+        </a> :
+        <div class={`card rank-${rank} ${suit}`}>
+          <span class='rank'>{rank}</span>
+          <span class='suit'>{unicodeSuit}</span>
+        </div>
+      }
     </li>
   );
 };
