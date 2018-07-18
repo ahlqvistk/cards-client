05ef4ede6b59f7cc895ed29b6e8e76994c64d429
ahlqvistk <kristoffer.ahlqvist@gmail.com>
2018-07-18 09:40:00 +0200

    Add Markers styling

--- src/components/markers.js
+++ src/components/markers.js
@@ -3,7 +3,9 @@ import {h} from 'hyperapp';
 export const Markers = ({active, bid, dealer, show}) => (
   <div class='markers'>
     {show ? <span class='bid'>Bid: {bid}</span> : null}
-    {active ? <span class='active'>A</span> : null}
-    {dealer ? <span class='dealer'>D</span> : null}
+    <span>
+      {active ? <span class='active'>A</span> : null}
+      {dealer ? <span class='dealer'>D</span> : null}
+    </span>
   </div>
 );
