7830de4dbe8a9a3932038ed074921350c89a5565
ahlqvistk <kristoffer.ahlqvist@gmail.com>
2018-07-18 08:47:01 +0200

    Fix player items positions

--- src/components/hand.js
+++ src/components/hand.js
@@ -2,14 +2,23 @@ import {h} from 'hyperapp';
 
 import {Card} from './card';
 
-export const Hand = ({cards}) => (
-  <div class='playingCards simpleCards'>
-    <ul class='hand'>
-      {cards.length ?
-        cards.map((card) => (
-          <Card card={card} inHand={true} key={card} />
-        )) : null
-      }
-    </ul>
-  </div>
-);
+export const Hand = ({cards}) => {
+  const width = 3.5 + 1.1 * cards.length;
+
+  return (
+    <div class='playingCards simpleCards'>
+      <ul
+        class='hand'
+        style={{
+          height: '7.05em',
+          width: `${width}em`,
+        }}>
+        {cards.length ?
+          cards.map((card) => (
+            <Card card={card} inHand={true} key={card} />
+          )) : null
+        }
+      </ul>
+    </div>
+  );
+};
--- src/components/player.js
+++ src/components/player.js
@@ -4,17 +4,31 @@ import {Card} from './card';
 import {Hand} from './hand';
 
 export const Player = ({active, cards, dealer, id, index, playedCard}) => (
-  <div class={'player player-' + index}>
-    {active ? <span class='active'>A</span> : null}
-    {dealer ? <span class='dealer'>D</span> : null}
-    {cards.length ?
-      <Hand cards={cards} /> :
+  index === 0 ?
+    <div class={'player player-' + index}>
+      {playedCard ?
+        <div class='playingCards simpleCards played-card'>
+          <Card card={playedCard} />
+        </div> : null
+      }
+      {active ? <span class='active'>A</span> : null}
+      {dealer ? <span class='dealer'>D</span> : null}
+      {cards.length ?
+        <Hand cards={cards} /> : null
+      }
       <div class='player-id'>{id}</div>
-    }
-    {playedCard ?
-      <div class='playingCards simpleCards played-card'>
-        <Card card={playedCard} />
-      </div> : null
-    }
-  </div>
+    </div> :
+    <div class={'player player-' + index}>
+      <div class='player-id'>{id}</div>
+      {cards.length ?
+        <Hand cards={cards} /> : null
+      }
+      {active ? <span class='active'>A</span> : null}
+      {dealer ? <span class='dealer'>D</span> : null}
+      {playedCard ?
+        <div class='playingCards simpleCards played-card'>
+          <Card card={playedCard} />
+        </div> : null
+      }
+    </div>
 );
