99b462cac788dd31837ef19d3e4107de02cbf4a5
ahlqvistk <kristoffer.ahlqvist@gmail.com>
2018-08-08 18:54:56 +0200

    Send enter password action to server

--- src/components/enter-password.js
+++ src/components/enter-password.js
@@ -1,10 +1,19 @@
 /* global document */
 import {h} from 'hyperapp';
+import {socket} from '../app';
+
+function enterPassword(password) {
+  return socket.emit('action', {
+    type: 'enter password',
+    payload: {hash: password},
+  });
+}
 
 function handlePass(e, setHash) {
   e.preventDefault();
   const password = document.getElementById('password').value;
   setHash(password);
+  enterPassword(password);
 }
 
 export function EnterPassword({setHash}) {
