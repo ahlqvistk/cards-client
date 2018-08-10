4da473c110da13020d4799f586c8c28e38a636cd
ahlqvistk <kristoffer.ahlqvist@gmail.com>
2018-08-10 21:00:49 +0200

    More Create table form styling

--- src/components/lobby/create-table.js
+++ src/components/lobby/create-table.js
@@ -13,31 +13,33 @@ export function CreateTable({toggleCreateTableModal}) {
         class='create-table-modal-back'
         onclick={() => toggleCreateTableModal()}></div>
       <form class='create-table-form' action='/table' method='post'>
-        <label for='id'>Table ID:</label>
-        <input type='text' id='id' name='id' />
+        <div>
+          <input type='text' name='id' placeholder='Table ID' />
+        </div>
 
-        <label for='public'>Public</label>
-        <input
-          type='radio'
-          id='public'
-          name='type'
-          onchange={() => togglePassword()}
-          value='public'
-          checked
-        />
+        <div>
+          <label for='public'>Public</label>
+          <input
+            type='radio'
+            id='public'
+            name='type'
+            onchange={() => togglePassword()}
+            value='public'
+            checked
+          />
 
-        <label for='private'>Private</label>
-        <input
-          type='radio'
-          id='private'
-          name='type'
-          value='private'
-          onchange={() => togglePassword()}
-        />
+          <label for='private'>Private</label>
+          <input
+            type='radio'
+            id='private'
+            name='type'
+            value='private'
+            onchange={() => togglePassword()}
+          />
+        </div>
 
         <div id='password-field' hidden>
-          <label for='password'>Password:</label>
-          <input type='password' id='password' name='password' />
+          <input type='password' name='password' placeholder='Password' />
         </div>
 
         <button type='submit'>Create table</button>
